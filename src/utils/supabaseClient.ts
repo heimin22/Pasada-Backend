f5861491313fbe7bc186b7523635e9d2fe862c01
heimin22 <fyketonel@gmail.com>
2025-04-16 14:30:23 +0800

    updating the supabase client

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,17 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supaabseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
+// Check if all required environment variables are set then exit if there are missing ones
+if (!supabaseUrl || !supaabseKey || !supabaseServiceRoleKey) {
+    console.error("Missing Supabase environment variables");
+    process.exit(1);
+}
+
+// client for general use, respects RLS
 export const supabase: SupabaseClient = createClient(supabaseUrl, supaabseKey);
