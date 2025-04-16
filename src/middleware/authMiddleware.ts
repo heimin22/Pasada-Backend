fc6a17a30b060e27703b6d160e6b6c2ad489083a
heimin22 <fyketonel@gmail.com>
2025-04-16 15:00:09 +0800

    middleware for the driver

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { getUserFromJWT } from "../utils/supabaseClient";
+import { getUserFromJWT, supabase } from "../utils/supabaseClient";
 
 // define a custom property 'user' on the request object
 declare global {
@@ -28,3 +28,42 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     req.user = user;
     next();
 }   
+
+// middleware for admins
+
+// middleware for drivers
+export const driverMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user?.id) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const userId = req.user.id;
+
+    try {
+        const { data, error, count } = await supabase
+            .from("driverTable")
+            .select("user_id", { count: "exact" , head: true})
+            .eq("user_id", userId)
+            .single();
+
+        if (error) {
+            console.error("Error fetching driver data:", error);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+
+        if (count && count > 0) {
+            next();
+        } else {
+            return res.status(403).json({ error: "Forbidden" });
+        }        
+    } catch (error) {
+        console.error("Error fetching driver data:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+// middleware for passengers
+export const passengerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    
+        
+}
