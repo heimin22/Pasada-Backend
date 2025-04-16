70c0d04895b89f52fbfda2cd166730baafd202c4
heimin22 <fyketonel@gmail.com>
2025-04-16 20:25:41 +0800

    `Added dependencies and updated package-lock.json`

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import driverRoutes from "./routes/driverRoutes";
+import tripRoutes from "./routes/tripRoutes";
+import passengerRoutes from "./routes/passengerRoutes";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Pasada Backend API is running");
+});
+
+app.use("/api/drivers", driverRoutes);
+app.use("/api/trips", tripRoutes);
+app.use("/api/passengers", passengerRoutes);
+
+app.use((err: any, req: Request, res: Response, next: Funciton) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
